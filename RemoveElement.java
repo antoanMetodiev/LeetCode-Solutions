3a0a2eb6c58a29ee5d46981ad950cee5e6fc8ae3
Antoan Metodiev <Boksnem@abv.bg>
2024-06-06 20:59:04 +0300

    add solution

---
+++ RemoveElement.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(removeElement(Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray()
+                , Integer.parseInt(scanner.nextLine())));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+
+    }
+}
