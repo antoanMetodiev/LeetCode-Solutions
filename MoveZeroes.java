3f1717732ecf095ca3636f53e9f50b20640a31fc
Antoan Metodiev <Boksnem@abv.bg>
2024-06-07 23:39:16 +0300

    add solution

---
+++ MoveZeroes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+       moveZeroes(Arrays.stream(new Scanner(System.in).nextLine().split(",")).mapToInt(Integer::parseInt).toArray());
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = nums[i];
+                nums[i] = temp;
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+}
