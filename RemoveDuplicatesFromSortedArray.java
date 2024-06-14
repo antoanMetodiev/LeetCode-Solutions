b68c7557d7da189d2c961f02d0b18135a84ba9ba
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-06-15 00:24:01 +0300

    Update RemoveDuplicatesFromSortedArray.java

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -8,11 +8,11 @@ public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int j = 1;
+        int index = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
-                j++;
+                nums[index] = nums[i];
+                index++;
             }
         }
         return j;
