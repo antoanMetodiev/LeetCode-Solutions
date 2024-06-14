34105ec302fcbe79e6c7ce0bbd5a28307f8d8fde
Antoan Metodiev <Boksnem@abv.bg>
2024-06-15 00:22:46 +0300

    add solution

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(Arrays.stream(new Scanner(System.in).nextLine().split(",")).mapToInt(Integer::parseInt).toArray()));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
