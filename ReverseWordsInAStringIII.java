f574da64064e9b0562fad35beb79e58e4c09b46a
Antoan Metodiev <Boksnem@abv.bg>
2024-06-14 22:40:51 +0300

    add solution

---
+++ ReverseWordsInAStringIII.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ReverseWordsInAStringIII {
+    public static void main(String[] args) {
+        System.out.println(reverseWords(new Scanner(System.in).nextLine()));
+    }
+
+    public static String reverseWords(String s) {
+        return Arrays.stream(s.split("\\s+")).map(word -> new StringBuilder(word).reverse().toString())
+                .collect(Collectors.joining(" "));
+    }
+
+}
