a0b900a67f5ce5c4c18278ec162a967ede5c5edb
Antoan Metodiev <Boksnem@abv.bg>
2024-06-16 21:41:17 +0300

    add solution

---
+++ FibonacciNumber.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FibonacciNumber {
+    private static final List<Integer> allFibNumbers = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println(fib(Integer.parseInt(new Scanner(System.in).nextLine())));
+    }
+
+    public static int fib(int n) {
+        if (n == 0) return 0;
+
+        allFibNumbers.add(0);
+        allFibNumbers.add(1);
+        for (int i = 2; i < n; i++) {
+            allFibNumbers.add(getFibonacci());
+        }
+
+        return allFibNumbers.get(allFibNumbers.size() - 1) + allFibNumbers.get(allFibNumbers.size() - 2);
+    }
+
+    private static int getFibonacci() {
+        return allFibNumbers.get(allFibNumbers.size() - 1) + allFibNumbers.get(allFibNumbers.size() - 2);
+    }
+}
