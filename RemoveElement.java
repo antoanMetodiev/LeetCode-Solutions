fc6b62caee98dfbf32c9d6c7983bef1c4322e6a9
Antoan Metodiev <Boksnem@abv.bg>
2024-08-15 21:31:17 +0300

    add solution

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,21 +1,27 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class RemoveElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(removeElement(Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray()
-                , Integer.parseInt(scanner.nextLine())));
+        System.out.println(removeElement((Arrays.stream(scanner.nextLine().split(","))
+                .mapToInt(Integer::parseInt).toArray()), Integer.parseInt(scanner.nextLine())));
     }
 
     public static int removeElement(int[] nums, int val) {
-        int k = 0;
+        List<Integer> notEqualNumbers = Arrays.stream(nums).filter(el -> el != val).boxed().collect(Collectors.toList());
+
+        int saveNotEqualNumbersInitSize = notEqualNumbers.size();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+            if (i <= saveNotEqualNumbersInitSize - 1) {
+                nums[i] = notEqualNumbers.remove(0);
+            } else {
+                nums[i] = val;
             }
         }
-        return k;
+
+        return saveNotEqualNumbersInitSize;
     }
 }
